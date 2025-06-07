ef2160cdf13835a16c0b52b02fd2b765c93cfe14
Pofkop <diondegroot555@hotmail.com>
2025-06-07 19:48:47 +0200

    Add files via upload

---
+++ scripts/assets/bitmonSprites.js
@@ -0,0 +1,23 @@
+// scripts/assets/bitmonSprites.js
+
+export const BitMonSprites = {
+  "Pofkop": {
+    name: "Pofkop",
+    type: "Normal",
+    level: 5,
+    spritePath: "assets/sprites/bitmon/pofkop.png"
+  },
+  "Vader": {
+    name: "Vader",
+    type: "Dark",
+    level: 5,
+    spritePath: "assets/sprites/bitmon/vader.png"
+  },
+  "DegenApe": {
+    name: "DegenApe",
+    type: "Fighting",
+    level: 5,
+    spritePath: "assets/sprites/bitmon/degenape.png"
+  },
+  // Additional BitMons can be added here following the same pattern
+};
