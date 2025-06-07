8625ad410d4589dad931eafac8287583346eb119
Pofkop <diondegroot555@hotmail.com>
2025-06-07 14:00:01 +0200

    Add files via upload

---
+++ scripts/ui/battleUI.js
@@ -0,0 +1,22 @@
+export function renderBattleScreen(ctx, playerMon, enemyMon) {
+  ctx.clearRect(0, 0, 480, 432);
+
+  // Background
+  ctx.fillStyle = '#1a1a1a';
+  ctx.fillRect(0, 0, 480, 432);
+
+  // Enemy
+  ctx.fillStyle = '#ff5555';
+  ctx.fillRect(300, 80, 100, 100);
+  ctx.fillStyle = '#fff';
+  ctx.fillText(enemyMon.name + ' HP: ' + enemyMon.hp, 280, 60);
+
+  // Player
+  ctx.fillStyle = '#55ff55';
+  ctx.fillRect(80, 250, 100, 100);
+  ctx.fillStyle = '#fff';
+  ctx.fillText(playerMon.name + ' HP: ' + playerMon.hp, 60, 240);
+
+  // Options
+  ctx.fillText('Press A to Attack', 150, 380);
+}
