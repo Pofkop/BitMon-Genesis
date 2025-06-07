907ef2448089c4e4d208dc80f0dc14f40ccdb422
Pofkop <diondegroot555@hotmail.com>
2025-06-07 15:07:51 +0200

    Add files via upload

--- scripts/modules/bitmon_region_map.js
+++ scripts/modules/bitmon_region_map.js
@@ -1,26 +1,24 @@
+// modules/bitmon_region_map.js
+
 export const REGION_MAPS = {
   'starter-town': {
-    map: [
-      [1, 1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 0, 1],
-      [1, 0, 1, 1, 0, 1],
-      [1, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1, 1]
-    ],
-    npcs: [
-      { x: 3, y: 2, name: 'Trainer Bob', text: 'Trainer Bob is trying to rug you, defend yourself!' }
-    ]
+    name: 'Starter Town',
+    width: 10,
+    height: 10,
+    getTile(x, y) {
+      const grassTiles = [ {x:3,y:3}, {x:4,y:4}, {x:5,y:5} ];
+      const isGrass = grassTiles.some(tile => tile.x === x && tile.y === y);
+      return isGrass ? { encounterRate: 0.2, region: 'starter-town' } : {};
+    }
   },
   'genesis-grove': {
-    map: [
-      [1, 1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 0, 1],
-      [1, 0, 1, 1, 0, 1],
-      [1, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1, 1]
-    ],
-    npcs: [
-      { x: 2, y: 3, name: 'Graeme', text: 'Graeme challenges you to a BitMon duel!' }
-    ]
+    name: 'Genesis Grove',
+    width: 10,
+    height: 10,
+    getTile(x, y) {
+      const grassTiles = [ {x:2,y:2}, {x:6,y:6}, {x:7,y:7} ];
+      const isGrass = grassTiles.some(tile => tile.x === x && tile.y === y);
+      return isGrass ? { encounterRate: 0.25, region: 'genesis-grove' } : {};
+    }
   }
 };
