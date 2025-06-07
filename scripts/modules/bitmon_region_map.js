cebfe21b666f54c27131b72f02490ccacc6b1d8e
Pofkop <diondegroot555@hotmail.com>
2025-06-07 14:24:10 +0200

    Add files via upload

---
+++ scripts/modules/bitmon_region_map.js
@@ -0,0 +1,26 @@
+export const REGION_MAPS = {
+  'starter-town': {
+    map: [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1]
+    ],
+    npcs: [
+      { x: 3, y: 2, name: 'Trainer Bob', text: 'Trainer Bob is trying to rug you, defend yourself!' }
+    ]
+  },
+  'genesis-grove': {
+    map: [
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 0, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1]
+    ],
+    npcs: [
+      { x: 2, y: 3, name: 'Graeme', text: 'Graeme challenges you to a BitMon duel!' }
+    ]
+  }
+};
