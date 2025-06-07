bcc6fa6bb6482a3566c0fc92611f6a0e0d40d89c
Pofkop <diondegroot555@hotmail.com>
2025-06-07 15:59:28 +0200

    Add files via upload

--- scripts/modules/bitmon.js
+++ scripts/modules/bitmon.js
@@ -1,39 +1,37 @@
 export class BitMon {
-  constructor(name, type, level = 1) {
+  constructor(name, type, level) {
     this.name = name;
     this.type = type;
     this.level = level;
-    this.maxHP = 20 + level * 5;
-    this.currentHP = this.maxHP;
-    this.moves = this.assignMoves();
+    this.hp = this.maxHp();
+    this.moves = this.startingMoves();
   }
 
-  assignMoves() {
-    const basicMoves = {
-      'Tackle': { power: 5 },
-      'Byte': { power: 6 },
-      'Ping': { power: 4 },
-      'Firewall': { power: 3 }
+  maxHp() {
+    return 20 + this.level * 5;
+  }
+
+  startingMoves() {
+    const moveset = {
+      Pofkop: ['Chatter', 'Fluff Up'],
+      Vader: ['Dark Pulse', 'Shadow Step'],
+      DegenApe: ['Smash', 'Rage'],
+      Virgen: ['Croak', 'Hop Blast'],
+      Ledgerbug: ['Firewall', 'Encrypt'],
+      Condz: ['Overclock', 'Cyber Punch'],
+      Graeme: ['Squirm Shot', 'Blue Hat'],
+      Francis: ['Code Jab', 'Deploy'],
+      Evansweb3dev: ['Anon Hack', 'Code Flood'],
+      Bigwil2k3: ['Bench Press', 'Crypto Flex'],
+      NickPlaysCrypto: ['VR Beam', 'Immersion'],
+      100xDarren: ['Cap Swipe', 'Alpha Dash'],
+      EtherMage: ['Arcane Byte', 'Dust Chain'],
+      Miratisu: ['Sweet Shot', 'Thread Flurry'],
+      Everythingempty: ['Dragon Code', 'Pixel Burst'],
+      VirtualsIntern: ['Hoodie Hop', 'Intern Swipe'],
+      Rugger: ['Drain', 'Rug Pull'],
+      Nullmon: ['Glitch', 'Crash'],
     };
-
-    const assigned = [];
-
-    const available = Object.keys(basicMoves);
-    for (let i = 0; i < Math.min(2 + Math.floor(this.level / 5), available.length); i++) {
-      assigned.push({
-        name: available[i],
-        ...basicMoves[available[i]]
-      });
-    }
-
-    return assigned;
-  }
-
-  isFainted() {
-    return this.currentHP <= 0;
-  }
-
-  takeDamage(amount) {
-    this.currentHP = Math.max(0, this.currentHP - amount);
+    return moveset[this.name] || ['Tackle'];
   }
 }
